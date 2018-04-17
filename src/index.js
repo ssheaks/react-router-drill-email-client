70a384828a3a820b939a317ca8d524c674797f09
ssheaks <sarah.sheaks@gmail.com>
2018-04-17 11:12:28 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 
 import Email from './components/email';
 import './index.css';
@@ -53,8 +54,10 @@ const store = createStore(state => state, initialState);
 
 
 ReactDOM.render(
+    <BrowserRouter>
     <Provider store={store}>
         <Email />
-    </Provider>,
+    </Provider>
+    </BrowserRouter>,
     document.getElementById('root')
 );
